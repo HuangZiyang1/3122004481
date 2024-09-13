de566f646f5f928fdbf437253166c03599f0ba7a
黄梓洋 <huangziyang@37.com>
2024-09-13 14:42:19 +0800

    feat: 执行性能分析，准备优化

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import os
+import cProfile
+
 
 # 定义一个函数来读取文本文件
 def read_texts_from_folder(folder_path):
@@ -12,22 +14,30 @@ def read_texts_from_folder(folder_path):
                 texts[filename] = file.read()
     return texts
 
+
 # 预处理和文本分割
 # 计算余弦相似度
 def calculate_cosine_similarity(texts):
     vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(texts.values())
-    original = X[0]  # 第一个是原始文件
-    similarities = cosine_similarity(original, X)
+    x = vectorizer.fit_transform(texts.values())
+    original = x[0]  # 第一个是原始文件
+    similarities = cosine_similarity(original, x)
     return texts.keys(), similarities[0]
+
 
 # 打印查重率
 def print_similarity_percentage(texts, similarities):
     for name, sim in zip(texts, similarities):
         print(f"{name}: {sim * 100:.2f}%")
 
-if __name__ == '__main__':
+
+def main():
     folder_path = 'text'  # 文本文件当前目录下的 'text' 文件夹中
     texts = read_texts_from_folder(folder_path)
     texts_keys, similarities = calculate_cosine_similarity(texts)
     print_similarity_percentage(texts_keys, similarities)
+    pass
+
+
+if __name__ == '__main__':
+    cProfile.run('main()', 'profile_output.main')
